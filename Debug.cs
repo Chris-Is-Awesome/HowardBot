2aba5fc75229d9c02f2dc8cfa0e7cea3074fcd39
Chris is Awesome <chrisstarkofficial@gmail.com>
2022-07-22 02:27:46 -0700

    Add helper methods

---
+++ Debug.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HowardPlays
+{
+    static class Debug
+    {
+        public static void Log(object message, bool includeTimestamp = true)
+        {
+            if (includeTimestamp)
+                Console.WriteLine($"{Utility.Timestamp} {message}\n");
+            else
+                Console.WriteLine(message + "\n");
+        }
+
+        public static void LogWarning(object message)
+        {
+            Console.WriteLine($"{Utility.Timestamp} WARN: {message}\n");
+        }
+
+        public static void LogError(object message)
+        {
+            Console.WriteLine($"{Utility.Timestamp} ERROR: {message}\n{new StackTrace()}\n");
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HowardPlays
+{
+	public static class Utility
+	{
+		public static string Timestamp
+		{
+			get
+			{
+				return $"[{DateTime.Now:hh:mm:ss.fff}]";
+			}
+		}
+
+		public static int GetRandomNumberInRange(int min, int max)
+		{
+			Random rand = new Random();
+			return rand.Next(min, max);
+		}
+
+		public static async Task WaitForSeconds(float seconds)
+		{
+			await Task.Delay(TimeSpan.FromSeconds(seconds));
+		}
+
+		public static async Task WaitForMilliseconds(float milliseconds)
+		{
+			await Task.Delay(TimeSpan.FromMilliseconds(milliseconds));
+		}
+	}
+}
