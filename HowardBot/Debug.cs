9d4b717ab70cc198bcfa31bb9bd537b92d4c6dd9
Chris-Is-Awesome <34765277+Chris-Is-Awesome@users.noreply.github.com>
2023-09-10 18:41:17 -0700

    Add colored console outputs

--- HowardBot/Debug.cs
+++ HowardBot/Debug.cs
@@ -3,39 +3,46 @@ using System.Diagnostics;
 
 namespace HowardBot
 {
-    static class Debug
-    {
-        /// <summary>
-        /// Logs a message to the console.
-        /// </summary>
-        /// <param name="message">The message to log</param>
-        /// <param name="includeTimestamp">If the timestamp should be included.</param>
-        public static void Log(object message, bool includeTimestamp = true)
-        {
-            if (includeTimestamp)
-                Console.WriteLine($"{Utility.Timestamp} {message}\n");
-            else
-                Console.WriteLine(message + "\n");
-        }
+	static class Debug
+	{
+		/// <summary>
+		/// Logs a message to the console.
+		/// </summary>
+		/// <param name="message">The message to log</param>
+		/// <param name="includeTimestamp">If the timestamp should be included.</param>
+		public static void Log(object message, bool includeTimestamp = true)
+		{
+			if (includeTimestamp)
+				Console.WriteLine($"{Utility.Timestamp} {message}\n");
+			else
+				Console.WriteLine(message + "\n");
+		}
 
-        /// <summary>
-        /// Logs a warning to the console.
-        /// </summary>
-        /// <param name="message">The message to log</param>
-        public static void LogWarning(object message)
-        {
-            Console.WriteLine($"{Utility.Timestamp} WARN: {message}\n");
-        }
+		/// <summary>
+		/// Logs a warning to the console.
+		/// </summary>
+		/// <param name="message">The message to log</param>
+		public static void LogWarning(object message)
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine($"{Utility.Timestamp} {message}\n");
+			Console.ResetColor();
+		}
 
-        /// <summary>
-        /// Logs an error to the console.
-        /// </summary>
-        /// <param name="message">The message to log</param>
-        public static void LogError(object message, bool postStackTrace = true)
-        {
-            string output = $"{Utility.Timestamp} ERROR: {message}\n";
-            if (postStackTrace) output += new StackTrace() + "\n";
-            Console.WriteLine(output);
-        }
-    }
+		/// <summary>
+		/// Logs an error to the console.
+		/// </summary>
+		/// <param name="message">The message to log</param>
+		public static void LogError(object message, bool postStackTrace = true)
+		{
+			string output = $"{Utility.Timestamp} {message}\n";
+
+			if (postStackTrace)
+				output += new StackTrace() + "\n";
+
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine(output);
+			Console.ResetColor();
+		}
+	}
 }
