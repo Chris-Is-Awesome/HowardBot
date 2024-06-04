621c7b787c8a618ab45ebbc26d7d75920b3fd59b
Chris is Awesome <chrisstarkofficial@gmail.com>
2024-06-04 09:01:21 -0700

    Add utility method for writing text files

--- HowardBot/Utility.cs
+++ HowardBot/Utility.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Configuration;
 using System.IO;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace HowardBot
 {
@@ -16,6 +16,14 @@ namespace HowardBot
 			}
 		}
 
+		public static string CurrentDirectory
+		{
+			get
+			{
+				return Directory.GetCurrentDirectory();
+			}
+		}
+
 		/// <summary>
 		/// Returns a random number between <paramref name="min"/> (inclusive) and <paramref name="max"/> (inclusive by default).
 		/// </summary>
@@ -25,7 +33,7 @@ namespace HowardBot
 		/// <returns>[int] The random number</returns>
 		public static int GetRandomNumberInRange(int min, int max, bool maxInclusive = true)
 		{
-			Random rand = new Random();
+			Random rand = new();
 			return rand.Next(min, maxInclusive ? max + 1 : max);
 		}
 
@@ -57,7 +65,7 @@ namespace HowardBot
 		{
 			using (StreamReader file = File.OpenText(filePath))
 			{
-				JsonSerializer serializer = new JsonSerializer();
+				JsonSerializer serializer = new();
 				return (T)serializer.Deserialize(file, typeof(T));
 			}
 		}
@@ -69,6 +77,36 @@ namespace HowardBot
 		public static string LoadEnvVar(string varName)
 		{
 			return ConfigurationManager.AppSettings[varName];
+		}
+
+		/// <summary>
+		/// Writes text to a text file. It will create the file and directory if they don't exist.
+		/// </summary>
+		/// <param name="directoryPath">The path to the parent directory containing the file</param>
+		/// <param name="fileName">The filename of the file, including extension</param>
+		/// <param name="text">The text to put in the file</param>
+		/// <param name="appendIfExists">Append text to the existing file instead of overriding the text</param>
+		public static void WriteToFile(string directoryPath, string fileName, string text, bool appendIfExists = true)
+		{
+			// Create the directory if it doesn't exist
+			if (!Directory.Exists(directoryPath))
+				Directory.CreateDirectory(directoryPath);
+
+			// Get full path
+			string fullPath = Path.Combine(directoryPath, fileName);
+
+			try
+			{
+				// Write to file
+				using (StreamWriter writer = new(fullPath, appendIfExists))
+				{
+					writer.WriteLine(text);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.Log($"Something went wrong when writing to file at path '{fullPath}'\n{ex.Message}");
+			}
 		}
 	}
 }
